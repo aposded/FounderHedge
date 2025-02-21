83e4f85b80bf3ff2f79e902aae861886f8c5e8fc
Apostolos Dedeloudis <apostolosded@gmail.com>
2025-02-21 09:05:55 -0500

    Cleaned up code

--- script/DeployUSDY.s.sol
+++ script/DeployUSDY.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {USDY} from "../src/USDY.sol";
+import {USDY} from "../src/SCR20/USDY.sol";
 
 contract DeployUSDY is Script {
     function run() external {
